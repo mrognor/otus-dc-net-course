d2ee26cf96f107859b5727b0083c450f2405dc62
mrognor <timurlego1@yandex.ru>
2026-04-26 20:04:51 +0300

    Add bfd to lab2

--- labs/lab2/configs/leaf2.sh
+++ labs/lab2/configs/leaf2.sh
@@ -12,21 +12,27 @@ ip address add 10.2.2.3/31 dev eth2
 cat << EOF > /etc/frr/frr.conf
 interface loopback0
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 interface eth1
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 interface eth2
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 router ospf 1
  passive-interface default
  router-id 10.0.0.2
+bfd
+ peer 10.0.0.2
+  no shutdown
 end
 EOF
--- labs/lab2/configs/spine2.sh
+++ labs/lab2/configs/spine2.sh
@@ -13,26 +13,33 @@ ip address add 10.2.2.4/31 dev eth3
 cat << EOF > /etc/frr/frr.conf
 interface loopback0
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 interface eth1
  no ip ospf passive
-ip ospf authentication message-digest
+ ip ospf bfd
+ ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 interface eth2
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 interface eth3
  no ip ospf passive
+ ip ospf bfd
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 ABCDEFGHIJK
  ip ospf area 0.0.0.0
 router ospf 1
  passive-interface default
  router-id 10.0.2.0
+bfd
+ peer 10.0.2.0
+  no shutdown
 end
 EOF
